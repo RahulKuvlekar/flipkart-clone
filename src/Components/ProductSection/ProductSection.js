50f952c6651ceac0ba3e8428a63bf95613367ad1
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-12-04 10:23:43 +0530

    Feat- created ProductSection component

---
+++ src/Components/ProductSection/ProductSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ProductSection.css";
+const ProductSection = () => {
+  return (
+    <div className="productsection">
+      <h1>Product Section</h1>
+    </div>
+  );
+};
+
+export default ProductSection;
