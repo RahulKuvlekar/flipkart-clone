57f520b5a3aa2fd8f4ae4e7069a1cff4ec032fbe
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-12-04 10:22:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FilterSidebar from "./Components/FilterSidebar/FilterSidebar";
+import Header from "./Components/Header/Header";
+import ProductSection from "./Components/ProductSection/ProductSection";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="product__body">
+        <FilterSidebar />
+        <ProductSection />
+      </div>
     </div>
   );
 }
