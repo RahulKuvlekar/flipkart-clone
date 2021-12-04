90e2072c9515d1ed414d5ca4c197da81f01bc36b
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-12-04 10:23:31 +0530

    Feat- created Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="navbar__section">
+      <h1>Flipkart Clone 🚀</h1>
+    </nav>
+  );
+};
+
+export default Header;
