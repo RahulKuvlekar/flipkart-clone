ed6d93254335df816f222a0e92f3f7fe9d515fca
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-12-04 10:23:20 +0530

    Feat- created FilterSidebar component

---
+++ src/Components/FilterSidebar/FilterSidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./FilterSidebar.css";
+const FilterSidebar = () => {
+  return (
+    <div className="fiterSidebar__section">
+      <h1>Filters</h1>
+    </div>
+  );
+};
+
+export default FilterSidebar;
